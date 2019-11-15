c15abab2f8b846369d161156453fb19a93f1c4c9
Modurotolu Olokode <durolawk@gmail.com>
2019-11-15 09:45:29 +0100

    setup authenticate middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,25 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(
+      token,
+      process.env.NODE_ENV === 'development' ? 'super secret' : process.env.SECRET,
+      (err, decodedToken) => {
+        if (err) {
+          res.status(401).json({ you: 'shall not pass!' });
+        } else {
+          req.decodedToken = decodedToken;
+          next();
+        }
+      }
+    )
+  } else {
+    res.status(400).json({ message: 'No credentials provided' });
+  }
 };
